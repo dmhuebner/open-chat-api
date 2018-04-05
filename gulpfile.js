636834fec1fbc756f4cf0d4c37fa7d821f5fcc57
David M <davidmh.mail@gmail.com>
2018-04-04 23:58:03 -0500

    Configure & implement first unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,31 @@ gulp.task('start', () => {
     ignore: '/.node_modules'
   })
   .on('restart', () => {
-    env({vars: {ENV: 'unit-test'}});
-    gulp.src('**/*spec.js', {read: false})
-      .pipe(gulpMocha({reporter: 'nyan'}));
+    console.log('Restarting...');
   });
 });
+
+gulp.task('test', () => {
+  runTests();
+});
+
+gulp.task('tdd', () => {
+  runTests();
+  nodemon({
+    script: 'app.js',
+    ext: 'js',
+    env: {
+      PORT: 8000
+    },
+    ignore: '/.node_modules'
+  })
+    .on('restart', () => {
+      runTests();
+    });
+});
+
+function runTests() {
+  env({vars: {ENV: 'unit-test'}});
+  gulp.src('**/*spec.js', {read: false})
+    .pipe(gulpMocha({reporter: 'nyan'}));
+}
