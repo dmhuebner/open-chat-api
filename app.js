81a9c7016554e3f7304151c90a0f1e31cf9b4afb
David M <davidmh.mail@gmail.com>
2018-07-10 01:02:26 -0500

    Adjust Access-Control-Allow-Methods header

--- app.js
+++ app.js
@@ -35,9 +35,10 @@ app.use(session({
 app.use(validator());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
-  res.header("Access-Control-Allow-Credentials", 'true');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   next();
 });
 
